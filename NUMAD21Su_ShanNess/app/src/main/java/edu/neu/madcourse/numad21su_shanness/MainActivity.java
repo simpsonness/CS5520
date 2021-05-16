f5a87fb53e2194b0f15a8426b6395f11147072c0
simpsonness <shanliness@gmail.com>
2021-05-16 16:38:08 +0100

    add current time to the app

--- NUMAD21Su_ShanNess/app/src/main/java/edu/neu/madcourse/numad21su_shanness/MainActivity.java
+++ NUMAD21Su_ShanNess/app/src/main/java/edu/neu/madcourse/numad21su_shanness/MainActivity.java
@@ -3,14 +3,24 @@ package edu.neu.madcourse.numad21su_shanness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView textview;
+    TextView textview, timeview;
     Button showBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +35,13 @@ public class MainActivity extends AppCompatActivity {
                 textview.setText("My name is Shan.\nMy email is 'shanliness@gmail.com'.");
             }
         });
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        String time = "Current time: " + format.format(calendar.getTime());
+
+        timeview = findViewById(R.id.time);
+        timeview.setText(time);
+
     }
 }
