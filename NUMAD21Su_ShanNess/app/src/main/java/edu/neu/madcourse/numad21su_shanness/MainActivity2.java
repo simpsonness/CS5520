bd807964643e9d5702abf0ac887082afa4c581d8
simpsonness <shanliness@gmail.com>
2021-05-30 13:19:06 +0100

    Add new button return back to the main page.

--- NUMAD21Su_ShanNess/app/src/main/java/edu/neu/madcourse/numad21su_shanness/MainActivity2.java
+++ NUMAD21Su_ShanNess/app/src/main/java/edu/neu/madcourse/numad21su_shanness/MainActivity2.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.numad21su_shanness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,7 +16,7 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button button1 = findViewById(R.id.button1);
+        Button button1 = findViewById(R.id.botton1);
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
         Button button4 = findViewById(R.id.button4);
@@ -25,31 +27,48 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
+        button5.setOnClickListener(this); 
         button6.setOnClickListener(this);
 
+    }
+
+    public void switchActivity(View view) {
+        Intent toy = new Intent(MainActivity2.this,MainActivity.class);
+        startActivity(toy);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button1:
-                Toast.makeText(this, "Pressed:A", Toast.LENGTH_SHORT).show();
+            case R.id.botton1:
+                Toast toast1 = Toast.makeText(this, "Pressed:A", Toast.LENGTH_SHORT);
+                toast1.setGravity(Gravity.LEFT, 0, -90);
+                toast1.show();
                 break;
             case R.id.button2:
-                Toast.makeText(this, "Pressed:B", Toast.LENGTH_SHORT).show();
+                Toast toast2 = Toast.makeText(this, "Pressed:B", Toast.LENGTH_SHORT);
+                toast2.setGravity(Gravity.LEFT, 0, -90);
+                toast2.show();
                 break;
             case R.id.button3:
-                Toast.makeText(this, "Pressed:C", Toast.LENGTH_SHORT).show();
+                Toast toast3 = Toast.makeText(this, "Pressed:C", Toast.LENGTH_SHORT);
+                toast3.setGravity(Gravity.LEFT, 0, -90);
+                toast3.show();
                 break;
             case R.id.button4:
-                Toast.makeText(this, "Pressed:D", Toast.LENGTH_SHORT).show();
+                Toast toast4 = Toast.makeText(this, "Pressed:D", Toast.LENGTH_SHORT);
+                toast4.setGravity(Gravity.LEFT, 0, -90);
+                toast4.show();
                 break;
             case R.id.button5:
-                Toast.makeText(this, "Pressed:E", Toast.LENGTH_SHORT).show();
+                Toast toast5 = Toast.makeText(this, "Pressed:E", Toast.LENGTH_SHORT);
+                toast5.setGravity(Gravity.LEFT, 0, -90);
+                toast5.show();
                 break;
             case R.id.button6:
-                Toast.makeText(this, "Pressed:F", Toast.LENGTH_SHORT).show();
+                Toast toast6 = Toast.makeText(this, "Pressed:F", Toast.LENGTH_SHORT);
+                toast6.setGravity(Gravity.LEFT, 0, -90);
+                toast6.show();
                 break;
 
         }
