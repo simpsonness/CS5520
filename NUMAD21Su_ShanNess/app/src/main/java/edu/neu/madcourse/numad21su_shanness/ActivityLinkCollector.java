9b81d0498f2c46e18b44d99c47cab057d5a6fb43
simpsonness <shanliness@gmail.com>
2021-06-11 16:23:39 +0100

    fixed the content view

--- NUMAD21Su_ShanNess/app/src/main/java/edu/neu/madcourse/numad21su_shanness/ActivityLinkCollector.java
+++ NUMAD21Su_ShanNess/app/src/main/java/edu/neu/madcourse/numad21su_shanness/ActivityLinkCollector.java
@@ -33,7 +33,7 @@ public class ActivityLinkCollector extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_link_collector);
 
         init(savedInstanceState);
 
