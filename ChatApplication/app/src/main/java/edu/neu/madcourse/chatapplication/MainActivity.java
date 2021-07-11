85192865693dda2d0944748c0129732eca81ab08
simpsonness <shanliness@gmail.com>
2021-07-11 15:56:29 +0100

    chat app basic function completed

--- ChatApplication/app/src/main/java/edu/neu/madcourse/chatapplication/MainActivity.java
+++ ChatApplication/app/src/main/java/edu/neu/madcourse/chatapplication/MainActivity.java
@@ -5,14 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mainActivityButton;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivityButton = (Button) findViewById(R.id.viewMainActivity2);
+        mainActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
     public void startSendStickerActivity(View view) {
         startActivity(new Intent(MainActivity.this, SendSticker.class));
@@ -21,4 +35,6 @@ public class MainActivity extends AppCompatActivity {
     public void startViewStickersActivity(View view) {
         startActivity(new Intent(MainActivity.this, ViewStickers.class));
     }
+
+
 }
